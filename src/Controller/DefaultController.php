924ec1d2aa8081ea6816aeeb15c5b1b0f32eb827
ArtKolev <mail@artkolev.ru>
2026-02-16 22:08:38 +0300

    add new code

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+final class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->redirectToRoute('form');
+    }
+}
---
+++ src/Controller/FormController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormController extends AbstractController
+{
+    /**
+     * @Route("/form", name="form")
+     */
+    public function index(): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('login');
+        }
+    }
+}
